c1ffac24b07baaff859ddf337fd61c22cd8309fc
lukmanyahya <105480771+lukmanyahya@users.noreply.github.com>
2023-03-09 20:55:02 +0700

    Update anu.php

--- mvc/app/Http/Controllers/anu.php
+++ mvc/app/Http/Controllers/anu.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\orang;
+use App\Models\orang;
 
 class anu extends Controller
 {
     //
 public function welcome(){
     $a['modelorang']=orang::all();
+  
     return view('welcome',$a);
 }
 }
